ed633cd4f1555da7db0b931702275e2ae45cf101
Penguin <say8425@gmail.com>
2019-07-06 18:25:01 +0900

    add rubocop and lint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,20 @@
-ENV["TEST"] = "1"
-ENV["JETS_ENV"] ||= "test"
+ENV['TEST'] = '1'
+ENV['JETS_ENV'] ||= 'test'
 # Ensures aws api never called. Fixture home folder does not contain ~/.aws/credentails
-ENV['HOME'] = "spec/fixtures/home"
+ENV['HOME'] = 'spec/fixtures/home'
 
-require "byebug"
-require "fileutils"
-require "jets"
+require 'byebug'
+require 'fileutils'
+require 'jets'
 
-abort("The Jets environment is running in production mode!") if Jets.env == "production"
+abort('The Jets environment is running in production mode!') if Jets.env == 'production'
 Jets.boot
 
-require "jets/spec_helpers"
-
-
+require 'jets/spec_helpers'
 
 module Helpers
   def payload(name)
-    JSON.load(IO.read("spec/fixtures/payloads/#{name}.json"))
+    JSON.parse(IO.read("spec/fixtures/payloads/#{name}.json"))
   end
 end
 
