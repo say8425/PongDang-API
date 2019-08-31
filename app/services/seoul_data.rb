891951d37d6e5b65bc4dcdb2a6e47d64653f86b7
Penguin <say8425@gmail.com>
2019-08-31 22:49:09 +0900

    add raise for tailing getting data

--- app/services/seoul_data.rb
+++ app/services/seoul_data.rb
@@ -7,6 +7,9 @@ class SeoulData
     response_body = JSON.parse(response.body)['WPOSInformationTime']['row']
     data = response_body.find { |key| key['SITE_ID'] == '노량진' }
 
+    # TODO: alert by telegram
+    raise 'Getting data is failed!' if data.blank?
+
     { temperature: data['W_TEMP'].to_f,
       measured_at: Time.parse("#{data['MSR_DATE']} #{data['MSR_TIME']} +09:00") }
   end
