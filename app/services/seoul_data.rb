c8b921379a5192dd2e0b866f775dab313ddb3c2a
Penguin <say8425@gmail.com>
2019-10-06 21:54:14 +0900

    JSON.parse is not needed
    use parse method

--- app/services/seoul_data.rb
+++ app/services/seoul_data.rb
@@ -4,7 +4,7 @@ class SeoulData
   def self.han_river_degree
     uri = "http://openapi.seoul.go.kr:8088/#{ENV['SEOUL_DATA_KEY']}/json/WPOSInformationTime/1/5".freeze
     response = HTTP.get(uri)
-    response_body = JSON.parse(response.body)['WPOSInformationTime']['row']
+    response_body = response.parse['WPOSInformationTime']['row']
     data = response_body.find { |key| key['SITE_ID'] == '노량진' }
 
     # TODO: alert by telegram
