04ebae1c42599025841906ac2fa270d32db2c073
Penguin <say8425@gmail.com>
2020-01-01 21:38:56 +0900

    add # frozen_string_literal: true to config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
-Jets.application.routes.draw do
-  root 'degrees#index'
-  get 'degrees', to: 'degrees#index'
-  get 'degrees/index', to: 'degrees#index'
+# frozen_string_literal: true
 
-  # The jets/public#show controller can serve static utf8 content out of the public folder.
-  # Note, as part of the deploy process Jets uploads files in the public folder to s3
-  # and serves them out of s3 directly. S3 is well suited to serve static assets.
-  # More info here: https://rubyonjets.com/docs/extras/assets-serving/
-  any '*catchall', to: 'jets/public#show'
+Jets.application.routes.draw do
+  root  'degrees#index'
+  get   'degrees',        to: 'degrees#index'
+  get   'degrees/index',  to: 'degrees#index'
+  any   '*catchall',      to: 'jets/public#show'
 end
